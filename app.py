d73b1d1c5f4d991e6384ee51b69c34ddcba08f46
Token-D <lkaterinmarin@gmail.com>
2025-04-01 17:07:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -467,10 +467,10 @@ if extracto_file and auxiliar_file:
         col1, col2 = st.columns(2)
         with col1:
             st.write("Primeras filas del extracto bancario:")
-            st.write(extracto_df.head(13))
+            st.write(extracto_df.head(30))
         with col2:
             st.write("Primeras filas del libro auxiliar:")
-            st.write(auxiliar_df.head(13))
+            st.write(auxiliar_df.head(30))
 
         # Realizar conciliación
         resultados_df = conciliar_banco_completo(extracto_df, auxiliar_df)
